bec7b738f3c33d76d21d9b9130e810cfb4211135
Otavio Gaiao <otavio.gaiao@al.go.leg.br>
2019-05-02 07:29:38 -0300

    initial commit

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp191b4319a58944b9b94555a7dfef20bd://172.16.3.241:19000";
+
+}
